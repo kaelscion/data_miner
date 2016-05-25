69918c0b9fd353de4e2aa0fe2669c61282a076ea
kaelscion <kaelscion@gmail.com>
2016-05-24 21:52:07 -0400

    testing how to draw ingredients out of recipe web page

--- recipe_miner.py
+++ recipe_miner.py
@@ -1,20 +1,25 @@
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
+import csv
 
 ingredients = []
 
-def get_recipe_data(link_data):
+def get_recipe_data(links):
         #for link in link_data:
-            html=urlopen(link_data).read()
+            html=urlopen(links).read()
             soup=BeautifulSoup(html,"lxml")
-            #get_recipe_title=soup.find('h1', {'class': 'entry_title'})
             recipe_title=soup.h1.get_text()
-            get_recipe_ingredients=soup.findAll(attrs={'class':'ingredient'})
+            get_recipe_ingredients=soup.findAll('p')
+            with open("C:/users/Jake/Desktop/contents.csv", 'w') as csvfile:
+                content_header=['Content']
+                writer=csv.DictWriter(csvfile, fieldnames=content_header)
+                writer.writeheader()
+                for content in get_recipe_ingredients:
+                    writer.writerow({'Content':str(content)})
             for ingredient in get_recipe_ingredients:
                 ingredient_soup=BeautifulSoup(str(ingredient), "lxml")
                 ingredients.append(ingredient_soup.get_text(strip=True))
-            #recipe_ingredients=[ul.li["class"] for ul in get_recipe_ingredients]
             print recipe_title
-            print ingredients
 
-get_recipe_data("http://sweetpotatochronicles.com/2014/01/vitamin-week-salmon-parcels/")
+        
+get_recipe_data("http://greatist.com/health/twice-baked-broccoli-and-kale-stuffed-potatoes")
